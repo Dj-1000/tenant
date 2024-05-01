7dfc79376b03c48f6ef06e3f33d999d39ba2873e
Dheeraj Sharma <djsharma1000@gmail.com>
2024-05-01 15:19:59 +0530

    added shipment views

--- organization/serializers.py
+++ organization/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Organization
+
 class OrgSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
--- shipments/views.py
+++ shipments/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ShipmentSerializer
+from rest_framework.exceptions import ValidationError
+from.models import Shipment
 
-# Create your views here.
+class ListShipment(generics.ListAPIView):
+    queryset = Shipment.objects.all()
+    serializer_class = ShipmentSerializer
+    
+    
+class ShipmentView(APIView):
+    def get(self,request,pk):
+        obj = Shipment.objects.filter(id = pk).first()
+        if not obj:
+            raise ValidationError({"shipment":"shipment does not exist"})
+
+        seri = ShipmentSerializer(obj)
+        return Response({
+            "status" : status.HTTP_200_OK,
+            "result" : seri.data
+        })
+        
+    def post(self,request):
+        seri = ShipmentSerializer(data = request.data)
+        if seri.is_valid():
+            seri.save()
+            return Response({
+                "status" : status.HTTP_201_CREATED,
+                "result_message" : "Shipment has been successfully registered"
+            })
+        else:
+            return Response({
+                "status" : status.HTTP_400_BAD_REQUEST,
+                "result_message" : seri.errors
+            })
+            
+    def put(self,request,pk):
+        obj = Shipment.objects.filter(id = pk).first()
+        if not obj:
+            raise ValidationError({"shipment":"shipment does not exist"})
+        
+        seri = ShipmentSerializer(obj,data = request.data)
+        if seri.is_valid():
+            seri.save()
+            return Response({
+                "status" : status.HTTP_200_OK,
+                "result_message" : "Shipment has been successfully updated"
+            })
+        else:
+            return Response({
+                "status" : status.HTTP_400_BAD_REQUEST,
+                "result_message" : seri.errors
+            })
+            
+    def delete(self,request,pk):
+        obj = Shipment.objects.filter(id = pk).first()
+        if not obj:
+            raise ValidationError({"shipment":"shipment does not exist"})
+        
+        obj.delete()
+        return Response({
+            "status" : status.HTTP_200_OK,
+            "result_message" : "Shipment has been successfully deleted"
+        })
+    
+
+
